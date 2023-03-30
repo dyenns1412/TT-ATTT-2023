29aaa55bd39d6a0216020b713cad543c09cc1c42
Yenns <112486004+dyenns1412@users.noreply.github.com>
2023-03-30 10:26:04 +0700

    Update c37_KMP.c

--- Câu 37/c37_KMP.c
+++ Câu 37/c37_KMP.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#define true 1
+#define false 0
+#define MAX 1000
 
-
-void  failure_Function(char* P, int* F)
+void failureFunction(char *P, int *F)
 {
-    int j = 2;// biến chạy số thứ tự trong bảng F
-    int i = 0;// Biến chạy vị trí trong chuỗi P
-    // mặc định vị trí 0 và 1 trong bảng là -1 và 0
-    F[0] = -1;
-    F[1] = 0;
-
-    //kiểm tra tiền tố và hậu tố trùng nhau
-    while(j < strlen(P))
-    {
+	int i = 0;
+	int j = 2;
+	F[0] = -1;
+	F[1] = 0;
+	
+	while(j < strlen(P))
+	{
+		
         if(P[j - 1] == P[i])
         {
             F[j] = i + 1;
@@ -29,82 +31,69 @@ void  failure_Function(char* P, int* F)
             F[j] = 0;
             j++;
         }
-    }
-
-    //Biểu diễn thông tin bảng
-    printf("Bang Failure Function: \n ");
-
-    for(int x = 1; x< 4*j; x++)
-    {
-        printf("-");
-    }
-    printf("\n");
-
-    for(int x = 0; x< j; x++)
-    {
-        printf("|%3d", F[x]);
-    }
-    printf("|\n ");
-
-     for(int x = 1; x< 4*j; x++)
-    {
-        printf("-");
-    }
-    printf("\n");
-
+	}
+	
+	printf("\nFailure function\n");
+	printf("%5s", "j");	
+	for (int i=0; i<strlen(P); i++)
+		printf("%5d", i);
+	printf("\n");
+	
+	printf("%5s", "F(j)");	
+	for(int k=0; k<j; k++)
+		printf("%5d", F[k]);
+	printf("\n");
 }
 
-int kmp(char* T, char* P, int* F)
+int KMPSearch (char *T, char *P, int *F)
 {
-
-    int i = 0, j= 0;
-
-    while(i+j < strlen(T))
-    {
-        if(P[j] == T[j+i])
-        {
-            j++;
-            if(j == strlen(P))
-            {
-                return i;
-            }
-        }
-
-        else
-        {
-            if(F[j] > -1)
-            {
-                j = F[j];
-                i += j - F[j];
-            }
-            else
-            {
-                j = 0;
-                i++;
-            }
-        }
-    }
-    return -1;
-
-
+	int i = 0, j = 0;
+	while(i < strlen(T))
+	{
+		if(T[i+j] == P[j])
+		{
+			j++;
+			if(j == strlen(P))
+				return i;
+		}
+		else
+		{
+			if(F[j] == -1)
+			{
+				i++;
+				j = 0;
+			}
+			else
+			{
+				i = i + j - F[i];
+				j = F[j];
+			}
+		}			
+	}
+	return -1;
 }
 
 
 int main()
 {
-    char T[256];
-    char P[256];
-    int F[256];
+	char T[MAX], P[MAX];
+	int F[MAX];
+	printf("Nhap chuoi T: ");
+	gets(T);
+	printf("Nhap chuoi P: ");
+	gets(P);
+		
+	failureFunction(P, F);	
+	if(KMPSearch(T, P, F) != -1)
+		printf("\nVi tri xuat hien cua P trong T la %d", KMPSearch(T, P, F));
+	else
+		printf("\nNot found!");
 
-    printf("Nhap chuoi T: ");
-    gets(T);
-
-    printf("Nhap chuoi P: ");
-    gets(P);
-    failure_Function(P,F);
-
-    if(kmp(T,P,F) != -1)
-        printf("Vi tri xuat hien P trong T la: %d", kmp(T,P,F));
-    else printf("Khong tim thay!!!");
-
+	return 0;
 }
+/*
+	Truong hop cai tien hon Boyer Moore
+	T = "aaaaaaaaaa"
+	P = "baaaa"
+*/
+
