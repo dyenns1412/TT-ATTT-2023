b3c72a8a294e4ce88ca7928144c194539fb0a67a
Yenns <112486004+dyenns1412@users.noreply.github.com>
2023-04-07 23:44:48 +0700

    Update c19_soNguyenthoamanGTBT.c

--- Câu 19/c19_soNguyenthoamanGTBT.c
+++ Câu 19/c19_soNguyenthoamanGTBT.c
@@ -1,39 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int isPrime(int a)
+int isPrime(int n)
 {
-    if (a == 1) return 0;
-    if (a == 2) return 1;
-    for (int i = 2; i*i <= a; i++)
+    if (n<2) return 0;
+    for (int i=2; i<=sqrt(n); i++)
     {
-        if (a % i == 0) return 0;
+        if (n%i==0) return 0;
     }
     return 1;
 }
 
 int main()
-{ 
-    int m,l;
-    scanf("%d %d", &m, &l);
-     while (m >= l || m < 0 || l <= 0)
+{
+    int a, b, c, m, l;
+    printf ("Nhap m, l (m<l): ");
+    scanf("%d%d", &m, &l);
+    printf ("Nhap A: ");
+    scanf("%d", &a);
+    printf ("Nhap B: ");
+    scanf("%d", &b);
+    printf ("Nhap C: ");
+    scanf("%d", &c);
+    for (int i=m; i<=l; i++)
     {
-        printf("Nhap 0 <= m < l!\n");
-        scanf("%d %d", &m, &l);
-    }
-    int  A, B, C;
-    scanf(" %d %d %d",  &A, &B, &C);
-    printf("A = %d; B = %d; C = %d\n", A, B, C);
-    int check = 0;
-    for (int i = m; i < l; i++)
-    {
-        if (isPrime(A*i*i + B*i + C) == 1)
+        int gt = a*i*i+b*i+c;
+        if (isPrime(gt)==1)
         {
-            printf("x=%d", i);
-            check = 1;
-            break;
+            printf("x = %d\n", i);
         }
     }
-    if(!check) printf("Khong co x thoa man");
-
-    return 0;
 }
