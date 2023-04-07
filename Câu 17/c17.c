383a1e0b5cbe404d8d5e0c50b6209010612f1e12
Yenns <112486004+dyenns1412@users.noreply.github.com>
2023-04-07 23:36:11 +0700

    Update c17.c

--- Câu 17/c17.c
+++ Câu 17/c17.c
@@ -1,35 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int isPrime(int a)
+int isPrime(int n)
 {
-    if (a == 1) return 0;
-    if (a == 2) return 1;
-    for (int i = 2; i*i <= a; i++)
+    if (n<2) return 0;
+    for (int i=2; i<=sqrt(n); i++)
     {
-        if (a % i == 0) return 0;
+        if (n%i==0) return 0;
     }
     return 1;
 }
 
 int main()
-{ 
-    int n;
-    scanf("%d", &n);
-    int  A, B, C;
-    scanf(" %d %d %d",  &A, &B, &C);
-    printf("A = %d; B = %d; C = %d\n", A, B, C);
-    int min=0,check=0;
-   
-    for (int i = 1; i < n; i++)
+{
+    int a, b, c;
+    printf ("Nhap A: ");
+    scanf("%d", &a);
+    printf ("Nhap B: ");
+    scanf("%d", &b);
+    printf ("Nhap C: ");
+    scanf("%d", &c);
+    for (int i=1; ; i++)
     {
-        if (isPrime(A*i*i + B*i + C) == 1)
+        int gt = a*i*i+b*i+c;
+        if (isPrime(gt)==1)
         {
-            printf("x = %d ", i);
-            check = 1;
+            printf("Xmin = %d", i);
             break;
         }
     }
-    if(!check) printf("Khong co x thoa man");
-
-    return 0;
 }
