b4b9d2e9b8578b484b20c37e3014e8edacc145e6
Yenns <112486004+dyenns1412@users.noreply.github.com>
2023-04-08 00:47:12 +0700

    Update c26_strongNumber.c

--- Câu 26/c26_strongNumber.c
+++ Câu 26/c26_strongNumber.c
@@ -1,39 +1,37 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 
-void solve(int n)
+int isPrime(int n)
 {
-    int prime[n+1];
-    for(int i=0; i<=n; i++)
-        prime[i] = 1;
-    prime[0] = prime[1] = 0;
-    for(int i=2; i<=sqrt(n); i++)
+    if(n<=1) return 0;
+    for(int i=2;ii<=n;i++)
     {
-        if(prime[i])
-        {
-            for(int j=ii; j<=n; j+=i)
-                prime[j] = 0;
-        }
+        if(n%i==0) return 0;
     }
-
-    for(int i=2; i<n; i++)
-    {
-        for(int j=2; j<=i; j++)
-        {
-            if(prime[j] && i%j==0 && i%(jj)==0)
-            {
-                printf("%d\n", i);
-                break;
-            }
-        }
-    }
+    return 1;
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    solve(n);
+    int n,check=0;
+    do{
+        scanf("%d",&n);
+    }while(n>=10000);
 
-    return 0;
+    for(int i = 1; i<n;i++)
+    {
+        for(int j = 1;j<i;j++)
+        {
+            if(isPrime(j)==1)
+            {
+                if(i%j==0 && i%(jj)==0)
+                {
+                     printf("\n%d ",i);
+                    //printf("\n%d chia het cho %d va %d ^ 2",i,j,j);
+                    check =1;
+                }
+            }
+        }
+    }
+    if(!check) printf("Khong co so S-num nho hon %d",n);
+
 }
