62a221c30cce7d33578b41854bd7d10f5056ad2e
Yenns <112486004+dyenns1412@users.noreply.github.com>
2023-04-07 23:37:45 +0700

    Update c10_Souocvauocnguyento.c

--- Câu 10/c10_Souocvauocnguyento.c
+++ Câu 10/c10_Souocvauocnguyento.c
@@ -1,30 +1,28 @@
-#include<stdio.h>
-#include<math.h>
-
-int isPrime(int n)
-{
-    if(n<2) return 0;
-    for(int i=2;i*i<=n;i++)
-    {
-        if(n%i==0) return 0;
-    }
-    return 1;
-}
-
-int main()
-{
-    int n, dem1=0, dem2=0;
-    scanf("%d", &n);
+#include <stdio.h>
+#include <math.h>
+int isPrime(long long int n){
+    if (n <= 1)
+        return 0;
+    for(int i =2;i*i<=n;i++)
+	{
+		if(n%i==0) return 0;
+	}
+	return 1;
     
-    for(int i=1;i<=n;i++)
-    {
-        if(n%i==0){
-            dem1+=1;
-            if(isPrime(i)==1)
-                dem2+=1;
-        }
+}
+int main(){
+    long long int n, d1=0, d2=0;
+    scanf("%lld",&n);
+    while(n<=0){
+        printf("Nhap n thoa man de bai: ");
+        scanf("%lld",&n);
     }
-    printf("So uoc cua N la: %d\nSo uoc nguyen to cua N la: %d",dem1,dem2);     
-
+    for(long long int i=1;i<=n;i++)
+        if(n%i==0){
+            d1+=1;
+            if(isPrime(i)==1)
+                d2+=1;
+        }
+    printf("So uoc cua N la: %lld\nSo uoc nguyen to cua N la: %lld",d1,d2);        
     return 0;
 }
